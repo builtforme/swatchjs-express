8ad0c2b475b6af0da96d61f9d19b2cca895f86c2
Fred <CodeFred@users.noreply.github.com>
2017-07-05 13:37:37 -0400

    Initial commit (#1)
    * Initial commit
    * Consuming the new API, adding POST support, 100% code coverage
    * Renaming project to "swatchjs-express"

---
+++ test/expose.test.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const swatch = require('swatchjs');
+const expose = require('../lib/expose');
+const handlers = require('../lib/handlers');
+
+function getApp(verbs) {
+  const app = {};
+  verbs.forEach(verb => app[verb] = sinon.spy());
+  return app;
+}
+
+function getModel() {
+  const model = swatch({
+    "add": {
+      handler: (a, b) => a + b,
+    },
+    "sub": {
+      handler: (a, b) => a - b,
+    },
+  });
+
+  return model;
+};
+
+describe('expose', () => {
+  it('should only register the requested verbs', () => {
+    const verbs = [ 'get' ];
+    const app = getApp(verbs);
+    const model = getModel();
+    const options = {
+      verbs: verbs,
+    };
+
+    expose(app, model, options);
+
+    expect(app.get.getCall(0).calledWith('/add')).to.be.true;
+    expect(app.get.getCall(1).calledWith('/sub')).to.be.true;
+  });
+
+  it('should register all verbs if no verbs were specified', () => {
+    const verbs = Object.keys(handlers);
+    const app = getApp(verbs);
+    const model = getModel();
+
+    expose(app, model);
+
+    verbs.forEach(verb => {
+      expect(app[verb].getCall(0).calledWith('/add')).to.be.true;
+      expect(app[verb].getCall(1).calledWith('/sub')).to.be.true;
+    });
+  });
+
+  it('should use the specified prefix', () => {
+    const verbs = Object.keys(handlers);
+    const app = getApp(verbs);
+    const model = getModel();
+    const prefix = 'api';
+    const options = {
+      prefix,
+    };
+
+    expose(app, model, options);
+
+    verbs.forEach(verb => {
+      expect(app[verb].getCall(0).calledWith(`/${prefix}/add`)).to.be.true;
+      expect(app[verb].getCall(1).calledWith(`/${prefix}/sub`)).to.be.true;
+    });
+  });
+});
