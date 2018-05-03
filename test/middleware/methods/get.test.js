130c7b9feb4543116a57f2bfac366e7062b297df
Michal Bryc <furchin@gmail.com>
2018-05-03 15:32:14 -0700

    Fix POST body handling

--- test/middleware/methods/get.test.js
+++ test/middleware/methods/get.test.js
@@ -3,12 +3,12 @@ const get = require('../../../lib/middleware/methods/get');
 
 describe('get', () => {
   it('returns ctx.query', () => {
-    const expressCtx = {
+    const req = {
       query: {
         a: 1,
         b: 2,
       },
     };
-    expect(get(expressCtx)).to.deep.equal(expressCtx.query);
+    expect(get(req)).to.deep.equal(req.query);
   });
 });
