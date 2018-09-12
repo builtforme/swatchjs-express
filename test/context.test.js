699644bb0e19329742fa3f51a0310da2c9a7d9d2
Brian Timm <bwtimm@gmail.com>
2018-09-11 22:17:13 -0700

    Add swatch properties for remaining express params

--- test/context.test.js
+++ test/context.test.js
@@ -6,11 +6,19 @@ const context = require('../lib/context');
 
 describe('context', () => {
   const req = {
+    get: (arg) => {
+      const headers = {
+        host: 'localhost:3123',
+        origin: 'localhost:3124',
+      };
+      return headers[arg];
+    },
     headers: { authorization: 'value' },
     method: 'GET',
+    originalUrl: '/some/path',
     path: 'some/path',
     protocol: 'http',
-    query: '?name=test',
+    query: { name: 'test' },
     secure: false,
   };
   const res = {};
@@ -28,11 +36,15 @@ describe('context', () => {
         expect(req.expressCtx).not.to.be.a('null');
 
         expect(req.expressCtx.swatchCtx.req.headers).to.deep.equal(req.headers);
+        expect(req.expressCtx.swatchCtx.req.href).to.deep.equal('http://localhost:3123/some/path');
+        expect(req.expressCtx.swatchCtx.req.host).to.deep.equal('localhost:3123');
         expect(req.expressCtx.swatchCtx.req.method).to.deep.equal('GET');
+        expect(req.expressCtx.swatchCtx.req.origin).to.deep.equal('localhost:3124');
         expect(req.expressCtx.swatchCtx.req.path).to.deep.equal('some/path');
         expect(req.expressCtx.swatchCtx.req.protocol).to.deep.equal('http');
-        expect(req.expressCtx.swatchCtx.req.query).to.deep.equal('?name=test');
+        expect(req.expressCtx.swatchCtx.req.query).to.deep.equal({ name: 'test' });
         expect(req.expressCtx.swatchCtx.req.secure).to.deep.equal(false);
+        expect(req.expressCtx.swatchCtx.req.url).to.deep.equal('some/path');
 
         // Make sure we can get the expressCtx from the swatchCtx
         expect(req.expressCtx.swatchCtx.expressCtx()).to.deep.equal(req.expressCtx);
